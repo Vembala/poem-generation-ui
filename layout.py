5f87045b410fc3ab4db4913735b4743c45240f9b
Sreerag M <sreeragibtl@gmail.com>
2022-03-07 05:34:51 +0530

    dev: add label in layout

--- layout.py
+++ layout.py
@@ -6,9 +6,9 @@ class VerticalBox(pyside2.QtWidgets.QVBoxLayout):
 
     """VertivalBox"""
 
-    def __init__(self, text_box: pyside2.QtWidgets.QWidget, enter_button: pyside2.QtWidgets.QWidget,):
+    def __init__(self, text_box: pyside2.QtWidgets.QWidget, output_label: pyside2.QtWidgets.QWidget,):
 
         """__init__"""
 
         self.addWidget(text_box,)
-        self.addWidget(enter_button,)
+        self.addWidget(output_label,)
